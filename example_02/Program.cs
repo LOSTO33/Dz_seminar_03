6700a4a967b610ae2aabde945aadd0d430cd238c
Pavel <pashaslovev.@mail.com>
2023-08-06 20:12:06 +1000

    Dz

---
+++ example_02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число:");
+string number = Console.ReadLine();
+int i = 0;
+bool IsPol = true;
+while(i<number.Length/2 && IsPol)
+{
+    if( number[i] != number[number.Length - i - 1])
+    {
+        IsPol= false;
+    }
+    i++;
+}
+if (IsPol)
+{
+    Console.WriteLine($"Число {number} Палиндром");
+}
+else
+{
+    Console.WriteLine($"Число {number} Не палиндром");
+}
---
+++ exapmle_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты Xa:");
+int numCoordXa = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите координаты Ya:");
+int numCoordYa = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите координаты Za:");
+int numCoordZa = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите координаты Xb:");
+int numCoordXb = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите координаты Yb:");
+int numCoordYb = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите координаты Zb:");
+int numCoordZb = int.Parse (Console.ReadLine());
+double numCoordAB = Math.Sqrt(Math.Pow(numCoordXb -numCoordXa,2) + Math.Pow(numCoordYb -numCoordYa,2)+ Math.Pow(numCoordZb -numCoordZa,2));
+System.Console.WriteLine($"Расстояние {numCoordAB}");
